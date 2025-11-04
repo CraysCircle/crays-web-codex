f617029146c03e020b6d5e94a807bd640eb86e73
Gurnoor <68712145+Gurn0or@users.noreply.github.com>
2025-11-04 08:46:15 +0530

    Add WalletProvider wrapper to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,5 @@
 import { Component, onCleanup, onMount } from 'solid-js';
-import { AccountProvider } from './contexts/AccountContext';
-import { connect, disconnect } from './sockets';
-import Toaster from './components/Toaster/Toaster';
-import { HomeProvider } from './contexts/HomeContext';
-import { ExploreProvider } from './contexts/ExploreContext';
-import { ThreadProvider } from './contexts/ThreadContext';
-import AppRouter from './Router';
-import { ProfileProvider } from './contexts/ProfileContext';
-import { SettingsProvider } from './contexts/SettingsContext';
-import { TranslatorProvider } from './contexts/TranslatorContext';
-import { NotificationsProvider } from './contexts/NotificationsContext';
-import { SearchProvider } from './contexts/SearchContext';
-import { MediaProvider } from './contexts/MediaContext';
-import { AppProvider } from './contexts/AppContext';
-import { ReadsProvider } from './contexts/ReadsContext';
-import { AdvancedSearchProvider } from './contexts/AdvancedSearchContext';
-import { DMProvider } from './contexts/DMContext';
-import WalletFab from './components/WalletFab';
-import 'media-chrome';
-import "media-chrome/media-theme-element";
-import 'hls-video-element';
-import 'videojs-video-element';
+import { WalletProvider } from './contexts/WalletContext';
 
 export const version = import.meta.env.PRIMAL_VERSION;
 export const APP_ID = `web_${version}_${Math.floor(Math.random()*10000000000)}`;
@@ -40,38 +19,40 @@ const App: Component = () => {
   });
 
   return (
-    <AppProvider>
-      <TranslatorProvider>
-        <Toaster>
-          <MediaProvider>
-            <AccountProvider>
-              <SearchProvider>
-                <AdvancedSearchProvider>
-                  <SettingsProvider>
-                    <ProfileProvider>
-                      <DMProvider>
-                        <NotificationsProvider>
-                          <ReadsProvider>
-                            <HomeProvider>
-                              <ExploreProvider>
-                                <ThreadProvider>
-                                  <AppRouter />
-                                </ThreadProvider>
-                              </ExploreProvider>
-                            </HomeProvider>
-                          </ReadsProvider>
-                        </NotificationsProvider>
-                      </DMProvider>
-                    </ProfileProvider>
-                  </SettingsProvider>
-                </AdvancedSearchProvider>
-              </SearchProvider>
-            </AccountProvider>
-          </MediaProvider>
-        </Toaster>
-      </TranslatorProvider>
-      {import.meta.env.VITE_SHOW_WALLET_ENTRY === 'true' ? <WalletFab /> : null}
-    </AppProvider>
+    <WalletProvider>
+      <AppProvider>
+        <TranslatorProvider>
+          <Toaster>
+            <MediaProvider>
+              <AccountProvider>
+                <SearchProvider>
+                  <AdvancedSearchProvider>
+                    <SettingsProvider>
+                      <ProfileProvider>
+                        <DMProvider>
+                          <NotificationsProvider>
+                            <ReadsProvider>
+                              <HomeProvider>
+                                <ExploreProvider>
+                                  <ThreadProvider>
+                                    <AppRouter />
+                                  </ThreadProvider>
+                                </ExploreProvider>
+                              </HomeProvider>
+                            </ReadsProvider>
+                          </NotificationsProvider>
+                        </DMProvider>
+                      </ProfileProvider>
+                    </SettingsProvider>
+                  </AdvancedSearchProvider>
+                </SearchProvider>
+              </AccountProvider>
+            </MediaProvider>
+          </Toaster>
+        </TranslatorProvider>
+        {import.meta.env.VITE_SHOW_WALLET_ENTRY === 'true' ? <WalletFAB /> : null}
+      </AppProvider>
+    </WalletProvider>
   );
 };
 
