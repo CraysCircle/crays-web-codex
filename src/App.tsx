6b5e540576a3fcea0baabe9babe69392c4c24ad0
Gurnoor <68712145+Gurn0or@users.noreply.github.com>
2025-11-04 08:49:23 +0530

    Add WalletProvider import and wrap App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 import { Component, onCleanup, onMount } from 'solid-js';
 import { WalletProvider } from './contexts/WalletContext';
+import AccountProvider from './contexts/AccountContext';
+import { connect, disconnect } from './sockets';
+import Toaster from './components/Toaster/Toaster';
+import HomeProvider from './contexts/HomeContext';
+import ExploreProvider from './contexts/ExploreContext';
+import ThreadProvider from './contexts/ThreadContext';
+import AppRouter from './Router';
+import ProfileProvider from './contexts/ProfileContext';
+import SettingsProvider from './contexts/SettingsContext';
+import TranslatorProvider from './contexts/TranslatorContext';
+import NotificationsProvider from './contexts/NotificationsContext';
+import SearchProvider from './contexts/SearchContext';
+import MediaProvider from './contexts/MediaContext';
+import AppProvider from './contexts/AppContext';
+import ReadsProvider from './contexts/ReadsContext';
+import AdvancedSearchProvider from './contexts/AdvancedSearchContext';
+import DMProvider from './contexts/DMContext';
+import WalletFAB from './components/WalletFab/WalletFab';
+
+import '@fontsource/inter/cyrillic.css';
+import '@fontsource/inter/greek.css';
+import '@fontsource/inter/latin.css';
 
 export const version = import.meta.env.PRIMAL_VERSION;
 export const APP_ID = `web_${version}_${Math.floor(Math.random()*10000000000)}`;
